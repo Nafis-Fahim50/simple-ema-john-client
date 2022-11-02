568ea3c7ccc17a4602e34c9fb7791710eb146070
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-02 22:06:16 +0600

    added tailwind css

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADhd1nxpIsoSMHrElD2vYUb5m6P-FFa1w",
+  authDomain: "simple-ema-john-d723b.firebaseapp.com",
+  projectId: "simple-ema-john-d723b",
+  storageBucket: "simple-ema-john-d723b.appspot.com",
+  messagingSenderId: "92524215585",
+  appId: "1:92524215585:web:34ddbb400825c11c321260"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
